5262e54ee33353932fb48978d93d474f60a38222
ThePhaseless <kukubaorch@gmail.com>
2024-10-24 13:06:46 +0200

    komentarze do lab1

--- lab1/example_1_1.asm
+++ lab1/example_1_1.asm
@@ -1,13 +1,13 @@
-section .txt
-	org 100H
-	
-start:
-	mov ah, 9
-	mov dx, message
-	int 21h
-	
-	mov ah, 4C00h
-	int 0x21
+section .txt          ; Sekcja tekstowa, która zawiera kod programu (instrukcje)
+	org 100H           ; Ustalenie punktu początkowego kodu na adresie 100h (typowe dla programów .COM)
 
-section .data
-	message db "Hello, World!$"
+start:                ; Etykieta początkowa (oznacza początek programu)
+	mov ah, 9         ; Ustawienie rejestru AH na 9 - funkcja DOS do wyświetlania łańcucha znaków
+	mov dx, message   ; Załadowanie adresu początku wiadomości (adres zmiennej message) do rejestru DX
+	int 21h           ; Wywołanie przerwania DOS 21h - wykonanie funkcji wyświetlania tekstu
+
+	mov ah, 4C00h     ; Funkcja DOS 4Ch - zakończenie programu. AH = 4Ch (funkcja), AL = 00h (kod wyjścia)
+	int 0x21          ; Wywołanie przerwania DOS 21h - zakończenie programu
+
+section .data         ; Sekcja danych, gdzie umieszczamy stałe i zmienne
+	message db "Hello, World!$" ; Definicja komunikatu do wyświetlenia. Znak '$' oznacza koniec łańcucha dla funkcji DOS
--- lab1/example_1_2.asm
+++ lab1/example_1_2.asm
@@ -1,29 +1,31 @@
-section .txt
-	org 100H
-	
-	mov ah, 9
-	mov dx, message
-	int 21H
-	
-	mov ah, 0ah
-	mov dx, input
-	int 21H
-	
-	mov ah, 9
-	mov dx, greeting
-	int 21H
-	
-	mov ah, 9
-	mov dx, input + 2
-	int 21H
-	
-	mov ah, 0x4C
-	int 21H
-	
-	
-section .data
-	message db "Input text: $"
-	input 	db 20
-			db 0
-			times 22 db "$"
-	greeting db 10, 13, 10, 13, "Hello: $"
+section .txt              ; Sekcja z kodem programu (instrukcje)
+	org 100H             ; Ustawienie punktu początkowego programu na adresie 100h
+
+	mov ah, 9            ; Funkcja DOS 9 - wyświetlenie łańcucha zakończonego znakiem '$'
+	mov dx, message      ; Załadowanie adresu zmiennej 'message' do rejestru DX
+	int 21H              ; Wywołanie przerwania DOS 21h - wyświetlenie tekstu "Input text:"
+
+	mov ah, 0ah          ; Funkcja DOS 0Ah - wprowadzenie ciągu znaków z klawiatury (do bufora w 'input')
+	mov dx, input        ; Załadowanie adresu bufora 'input' do rejestru DX
+	int 21H              ; Wywołanie przerwania DOS 21h - czekanie na wprowadzenie tekstu przez użytkownika
+
+	mov ah, 9            ; Funkcja DOS 9 - wyświetlenie kolejnego łańcucha zakończonego znakiem '$'
+	mov dx, greeting     ; Załadowanie adresu zmiennej 'greeting' do rejestru DX
+	int 21H              ; Wywołanie przerwania DOS 21h - wyświetlenie tekstu powitalnego "Hello:"
+
+	mov ah, 9            ; Funkcja DOS 9 - wyświetlenie wprowadzonego przez użytkownika tekstu
+	mov dx, input + 2    ; Wprowadzone znaki zaczynają się od trzeciego bajtu w buforze 'input' (dlatego +2)
+	int 21H              ; Wywołanie przerwania DOS 21h - wyświetlenie tekstu wprowadzonego przez użytkownika
+
+	mov ah, 0x4C         ; Funkcja DOS 4Ch - zakończenie programu
+	int 21H              ; Wywołanie przerwania DOS 21h - zakończenie działania programu
+
+
+section .data             ; Sekcja danych programu
+	message db "Input text: $"      ; Komunikat wyświetlany przed wprowadzeniem tekstu przez użytkownika
+
+	input 	db 20                  ; Maksymalna liczba znaków do wprowadzenia
+	        db 0                   ; Bieżąca liczba wprowadzonych znaków (na początku 0)
+	        times 22 db "$"        ; Bufor na wprowadzone znaki, plus znak '$' (zapas na 20 znaków + kończący)
+
+	greeting db 10, 13, 10, 13, "Hello: $" ; Tekst powitalny "Hello:" poprzedzony znakami nowej linii i powrotu karetki (CRLF)
