be1a5523b9927af6ab61cf7c003ab7ece1f52e76
Thephaseless <kukubaorch@gmail.com>
2024-12-05 12:35:04 +0000

    move from examples to code

--- lab8/main.c
+++ lab8/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-extern char *_process_string(char *str, int len);
-char *process_string(char *str, int len);
+extern char *process_string(char *str, int len);
 
-extern int _count_chars(char *str);
-int count_chars(char *str);
+extern int count_chars(char *str);
 
 int count_len(char *str)
 {
@@ -26,14 +24,18 @@ int main()
 	int len;
 	char str[100];
 	print_line();
+
 	printf("Input string: ");
 	scanf("%s", str);
+
 	len = count_len(str);
 	printf("Before: %s", str);
 	print_line();
+
 	process_string(str, len);
 	printf(" After: %s", str);
 	print_line();
+
 	printf("Count chars: %d", count_chars(str));
 	print_line();
 	return 0;
